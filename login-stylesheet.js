bcb10cf659d22d1c6dcc71205a0b151709c431c4
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-12-22 15:50:35 +0530

    Create login-stylesheet.js

---
+++ login-stylesheet.js
@@ -0,0 +1,58 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appIcon: {
+    width: RFValue(130),
+    height: RFValue(130),
+    resizeMode: "contain"
+  },
+  appTitleText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  buttonContainer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: RFValue(250),
+    height: RFValue(50),
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: RFValue(30),
+    backgroundColor: "white"
+  },
+  googleIcon: {
+    width: RFValue(30),
+    height: RFValue(30),
+    resizeMode: "contain"
+  },
+  googleText: {
+    color: "black",
+    fontSize: RFValue(20),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cloudContainer: {
+    flex: 0.3
+  },
+  cloudImage: {
+    position: "absolute",
+    width: "100%",
+    resizeMode: "contain",
+    bottom: RFValue(-5)
+  }
+});
